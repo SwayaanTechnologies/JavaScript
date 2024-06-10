46d828e1e47e3d4a9121c66146e5a40d339548bb
Muralitharan Yoganath <muraliemailz@gmail.com>
2024-06-10 11:06:42 +0530

    Deleted the file

--- JavaScriptNodeRuntimeExamples/JS147-ES6-Modules.js
+++
@@ -1,7 +0,0 @@
-const { PI } = Math;
-
-export function area(r) { return PI * r ** 2; }
-export function circumference(r) { return 2 * PI * r; }
-// foo.js
-import { area } from './JS146-ES6-Classes.js';
-console.log(`The area of a circle of radius 4 is ${area(4)}`);
